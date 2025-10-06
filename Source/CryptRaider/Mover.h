618bf8b38be9d7581a62e1229d3f939d5d33736c
ThiagoIanuch <thiagoianuch@hotmail.com>
2025-10-05 22:30:08 -0300

    Add component to move door

---
+++ Source/CryptRaider/Mover.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Mover.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CRYPTRAIDER_API UMover : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UMover();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	FVector OriginalLocation{};
+
+	UPROPERTY(EditAnywhere);
+	FVector MoveOffSet{ 0, 0, -600 };
+
+	UPROPERTY(EditAnywhere);
+	float MoveTime = 10;
+
+	UPROPERTY(EditAnywhere);
+	bool ShouldMove{ false };
+
+};
